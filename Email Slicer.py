3bddda9166ec54e4854778052cef82a8f9d2a6ef
SandyJulianto <107165217+SandyJulianto@users.noreply.github.com>
2022-07-07 15:34:31 +0700

    Add comments
    Add some comments

--- Email Slicer.py
+++ Email Slicer.py
@@ -1,9 +1,13 @@
 # Email Slicer
+# Input email
 ser_email = input("Enter your email : ")
 
+# Create new list
 split_email = []
 
+# Split the email by "@"
 for i in user_email.split("@"):
+    # Add i to split_email
     split_email.append(i)
 
 print("Your email name is {} and your domain is {}".format(split_email[0], split_email[-1]))
